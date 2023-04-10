152c2a614d66f870fc96ec6ee802393b3d59fcc0
botaro <binbotaro3@gmail.com>
2023-04-10 13:20:01 +0900

    2023-04-10-13-20-01

--- 01/php/vie/par/p2.php
+++ 01/php/vie/par/p2.php
@@ -1,10 +1,13 @@
-<div class=card id=p2>
+<div class=card id=p2-<?php echo $_POST["tip"];?> >
 <div class=card-body>
 
 <br>
 <br>
 <div class="btn btn-bl2">
 p2</div>
+<?php
+echo $_POST["tip"];
+?> 
 
 <br>
 <br>
--- 01/php/vie/php/tip2.php
+++ 01/php/vie/php/tip2.php
@@ -3,7 +3,7 @@ for($j=0;$j<$cnt;$j++){
 $r=$ar[$j][0];
 echo "<div class='btn btn-info'>";
 echo "<form action=#p2-".$r." method=post>";
-echo "<input type=hidden value=".$r.">";
+echo "<input type=hidden name=tip value=".$r.">";
 echo "<input type=submit value=".$r.">";
 
 echo "</form>";
