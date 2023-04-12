abc5a794cca1826a81cb8166d3e6b30e64d68974
botaro <binbotaro3@gmail.com>
2023-04-12 11:30:53 +0900

    2023-04-12-11-30-53

--- 01/php/vie/php/tip1.php
+++ 01/php/vie/php/tip1.php
@@ -1,13 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-//$d=getcwd();
-=======
-$ver=SQLite3::version();
-
-$ve2=$ver["versionString"];
 $d=getcwd();
->>>>>>> 0f8b8d8465ef8fa0dc619cd063650240a83ec75b
 $dbn=$d."/vie/php/db/go.db";
 
 $db=new SQLite3($dbn);
