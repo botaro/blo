cbc17b0c0450c7de3219cc40c13c8799d0f0a831
botaro <binbotaro3@gmail.com>
2023-04-21 22:34:46 +0900

    2023-04-21-22-34-46

--- js/ini.js
+++ js/ini.js
@@ -1,8 +1,21 @@
-let sel=id=>document.querySelector(id)
-    ,gId=id=>document.getElementById(id)
-    ,crE=id=>document.createElement(id)
-    ,cl=arg=>console.log(arg)
-
 let Esc=fun=>addEventListener("scroll",e=>fun)
 let Ecl=fun=>addEventListener("click",e=>fun)
+
+let sel=a=> document.querySelector(a)
+,cl=a=> console.log(a)
+,crE=id=> document.createElement(id)
+
+let crT=a=> document.createTextNode(a)
+,gId=id=> document.getElementById(id)
+,gCl=n=> document.getElementsByClassName(n)
+,gTg=id=> document.getElementsByTagName(id)
+,inH=(e,a)=> e.innerHTML=a
+,clA=(e,a)=> e.classList.add(a)
+,clR=(e,a)=> e.classList.remove(a)
+,stA=(e,a,b)=> e.setAttribute(a,b)
+,dtS=(e,a,b)=> e.dataset.a=b
+,apC=(e,a)=> e.appendChild(a)
+,rmC=(e,a)=> e.removeChild(a)
+,adC=(e,f)=>e.addEventListener("click",f)
+
 
