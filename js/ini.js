f9f579b0b31b27aee108ed79c0ff1d51e6b48ca1
botaro <binbotaro3@gmail.com>
2023-05-04 18:29:21 +0900

    2023-05-04-18-29-21

--- js/ini.js
+++ js/ini.js
@@ -1,16 +1,18 @@
 
 //let sel=a=> document.querySelector(a)
 let cl=a=> console.log(a)
-,crE=id=> document.createElement(id)
+,crE=n=> document.createElement(n)
+,crD=document.createElement("div")
 
 let crT=a=> document.createTextNode(a)
 ,gId=id=> document.getElementById(id)
 ,gCl=n=> document.getElementsByClassName(n)
-,gTn=id=> document.getElementsByTagName(id)
+,gTn=n=> document.getElementsByTagName(n)
 ,inH=(e,a)=> e.innerHTML=a
 ,clA=(e,a)=> e.classList.add(a)
 ,clR=(e,a)=> e.classList.remove(a)
 ,stA=(e,a,b)=> e.setAttribute(a,b)
+,stC=(e,a)=> e.setAttribute("card",b)
 ,dtS=(e,a,b)=> e.dataset.a=b
 ,apC=(e,a)=> e.appendChild(a)
 ,rmC=(e,a)=> e.removeChild(a)
